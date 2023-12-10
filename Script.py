b06e348243dc8dac37eb0e9e23e9e5847271f798
Zziwadavid51 <149446130+Zziwadavid51@users.noreply.github.com>
2023-12-10 22:31:27 +0300

    Update Script.py
    caption

--- Script.py
+++ Script.py
@@ -194,7 +194,8 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 : {file_name}
+    CAPTION = """<b>📂 : <code>{file_name}</code>
+➢ <a href=https://t.me/+kIHJBYxTUqhjNzg0>𝐌𝐚𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥📡</a>
 </b>""" 
 
     IMDB_TEMPLATE_TXT = """
