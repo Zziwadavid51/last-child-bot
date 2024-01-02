4487147ecb5745ba27e71886951ac2881f2f8ad6
Zziwadavid51 <149446130+Zziwadavid51@users.noreply.github.com>
2024-01-02 12:10:51 +0300

    Update info.py

--- info.py
+++ info.py
@@ -97,4 +97,4 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 # for jenny chatGPT 
-OPENAI_API = environ.get('OPENAI_API', 'https://ajinkya.link/api/gpt.php?text=') 
+OPENAI_API = environ.get('OPENAI_API', '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') 
